7d3824bf72a6c7980ead9ff435cf5b2c6d688ea7
Feavy <34109688+Feavy@users.noreply.github.com>
2021-08-02 17:15:26 +0200

    Fix issue for case like <stop/>

--- src/main/java/fr/feavy/xml/XMLParser.java
+++ src/main/java/fr/feavy/xml/XMLParser.java
@@ -118,6 +118,9 @@ public class XMLParser {
             int index = line.indexOf(" ");
             if (index < 0) {
                 index = line.indexOf(">");
+                if(index-1 >= 0 && line.charAt(index-1) == '/') {
+                    index--;
+                }
             }
             if (index < 0) {
                 index = line.indexOf("/");
